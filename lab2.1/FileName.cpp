8d32fd8af15546a267f02119806df2a36d533854
Fudzikawa <fdzkwww@gmail.com>
2023-10-17 17:57:51 +0300

    first master commit

---
+++ lab2.1/FileName.cpp
@@ -0,0 +1,35 @@
+﻿// <Siliava Oleksandr>
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 26
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double a;
+
+    // Введення значення a від користувача
+    std::cout << "Введіть значення a: ";
+    std::cin >> a;
+
+    // Обчислення змінних z1 і z2 за заданими формулами
+    double z1 = cos(a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
+    double z2 = 4 * cos(a / 2) * cos(5 * a / 2) * cos(4 * a);
+
+    // Виведення результатів на екран
+    std::cout << "z1 = " << z1 << std::endl;
+    std::cout << "z2 = " << z2 << std::endl;
+
+    
+
+    return 0;
+}
